71d2eea2a0940daf701d3c2b194ba1823a88a08b
KarenC7 <40868869+KarenC7@users.noreply.github.com>
2021-02-08 02:34:10 -0600

    Creare a simple test to validate DateFormat

--- src/test/java/com/albo/test/TestApplicationTests.java
+++ src/test/java/com/albo/test/TestApplicationTests.java
@@ -1,8 +1,11 @@
 package com.albo.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class TestApplicationTests {
 
@@ -11,3 +14,7 @@ class TestApplicationTests {
 	}
 
 }
+
+
+
+
