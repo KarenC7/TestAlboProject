0636e7bfc32df0389c1252a880999eab1ab88e44
KarenC7 <40868869+KarenC7@users.noreply.github.com>
2021-02-08 09:27:33 -0600

    Create getInformation.sh to execute jar
    Delete test to avoid failure on Jar

---
+++ getInformation.sh
@@ -0,0 +1,15 @@
+#****************************************************************************************#
+#*                                                                                      *#
+#*  DESCRIPCION          : EJECUCIÓN DE FLUJO PARA CREAR PROCESO DE NÓMINA CORRES-      *#
+#*                         PONDIENTE AL CALENDARIO DEL DÍA ACTUAL O MENOR QUE AÚN       *#
+#*                         NO INICIA.                                                   *#
+#*                                                                                      *#
+#****************************************************************************************#
+
+#!/bin/bash
+
+FILE_JAR="test-0.0.1-SNAPSHOT.jar"
+
+java -jar $FILE_JAR
+
+exit
--- src/test/java/com/albo/test/TestApplicationTests.java
+++ src/test/java/com/albo/test/TestApplicationTests.java
@@ -1,11 +1,10 @@
 package com.albo.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class TestApplicationTests {
 
