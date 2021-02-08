a7dceaa217dc90b01727bad28a4268d5c259dc2a
KarenC7 <40868869+KarenC7@users.noreply.github.com>
2021-02-08 01:31:43 -0600

    Add new Rest Api to get Characters and Comics

--- src/main/java/com/albo/test/controllers/ShowController.java
+++ src/main/java/com/albo/test/controllers/ShowController.java
@@ -19,6 +19,8 @@ import com.albo.test.models.services.ICharactersService;
 //Permite que se llamen las APIS desde el dominio de prueba
 @CrossOrigin(origins = {"http://test.albo.mx/"})
 
+//Urls solicitadas para mostrar por personaje
+
 @RestController
 @RequestMapping("/marvel")
 public class ShowController {
@@ -26,14 +28,52 @@ public class ShowController {
 	@Autowired
 	private ICharactersService catService;
 	
+	/**
+	 * @desc ruta generalpara mostrar colaboradores por rol
+	 * @param String catalog - el catalogo personalizado a ser consultado
+	 * @return ResponseEntity<?>(JSON del registro de la entidad, estatus de respuesta) 
+	*/
 	@GetMapping("/collaborators/{character}")
-	public ResponseEntity<?> listCharacters(@PathVariable String character){
+	public ResponseEntity<?> listCollaborators(@PathVariable String character){
 		Object catEntity = null;
 		Map<String, Object> response = new HashMap<>();
 		try {
 			
 			//Llamada al servicio
 			catEntity = catService.findByName(character);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch(IllegalArgumentException mapExcepcion) {
+			response.put("mensaje", "Error al mapear datos.");
+			response.put("error", mapExcepcion.getMessage());
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		if(catEntity == null) {
+			response.put("mensaje", "No existen elementos.");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Object>(catEntity, HttpStatus.OK);
+
+	
+	}
+	
+	/**
+	 * @desc ruta general para mostrar comics por personaje relacionado
+	 * @param String catalog - el catalogo personalizado a ser consultado
+	 * @return ResponseEntity<?>(JSON del registro de la entidad, estatus de respuesta) 
+	*/
+	@GetMapping("/characters/{character}")
+	public ResponseEntity<?> listCharacters(@PathVariable String character){
+		Object catEntity = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			
+			//Llamada al servicio
+			catEntity = catService.findCharacter(character);
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
