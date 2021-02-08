072d9db6517106b2c720cc22163e44d1f5342571
KarenC7 <40868869+KarenC7@users.noreply.github.com>
2021-02-08 01:27:43 -0600

    Add new entities to Save Comics, Related Characters and relations

---
+++ src/main/java/com/albo/test/models/entities/CharXRelatedCharacterXComicIdentity.java
@@ -0,0 +1,55 @@
+package com.albo.test.models.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class CharXRelatedCharacterXComicIdentity implements Serializable {
+	 
+	private static final long serialVersionUID = 1L;
+	
+	private Long idCharacter;
+	
+    private Long idRelated;
+    
+    private Long idComic;
+    
+    
+	public CharXRelatedCharacterXComicIdentity()  {
+
+    }
+
+    public CharXRelatedCharacterXComicIdentity(Long idCharacter, Long idRelated,Long idComic) {
+        this.idCharacter = idCharacter;
+        this.idRelated = idRelated;
+        this.idComic = idComic;
+    }
+
+	public Long getIdCharacter() {
+		return idCharacter;
+	}
+
+	public void setIdCharacter(Long idCharacter) {
+		this.idCharacter = idCharacter;
+	}
+
+	public Long getIdRelated() {
+		return idRelated;
+	}
+
+	public void setIdRelated(Long idRelatedCharacter) {
+		this.idRelated = idRelatedCharacter;
+	}
+
+	public Long getIdComic() {
+		return idComic;
+	}
+
+	public void setIdComic(Long idComic) {
+		this.idComic = idComic;
+	}
+    
+    
+
+}
---
+++ src/main/java/com/albo/test/models/entities/Comic.java
@@ -0,0 +1,35 @@
+package com.albo.test.models.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comic")
+public class Comic{
+	@Id
+	@Column(name = "ID_COMIC", nullable = false)
+	private Long idComic;
+	@Column(name = "NAME")
+	private String name;
+	
+	
+	public Long getIdComic() {
+		return idComic;
+	}
+	public void setIdComic(Long idComic) {
+		this.idComic = idComic;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+}
